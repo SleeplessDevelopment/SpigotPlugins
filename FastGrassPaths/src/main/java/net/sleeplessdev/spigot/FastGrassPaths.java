c00249000e0c05d8cbd65869b5ca18bafe74f30b
InsomniaKitten <admin@designdevils.co.uk>
2018-07-23 21:32:29 +0100

    Migrate from plugin.yml resource to annotation system

--- FastGrassPaths/src/main/java/net/sleeplessdev/spigot/FastGrassPaths.java
+++ FastGrassPaths/src/main/java/net/sleeplessdev/spigot/FastGrassPaths.java
@@ -5,9 +5,15 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+@Plugin(name = "fastgrasspaths", version = "@version@")
+@Author("InsomniaKitten")
+@ApiVersion(ApiVersion.Target.v1_13)
 public final class FastGrassPaths extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
--- RightClickCrops/src/main/java/net/sleeplessdev/spigot/RightClickCrops.java
+++ RightClickCrops/src/main/java/net/sleeplessdev/spigot/RightClickCrops.java
@@ -10,7 +10,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
+@Plugin(name = "rightclickcrops", version = "@version@")
+@Author("InsomniaKitten")
+@ApiVersion(ApiVersion.Target.v1_13)
 public final class RightClickCrops extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
